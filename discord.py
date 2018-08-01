a78c49645e2bf9f44f09061cb43512d447285dbd
JosephVolosin <joeyvolosin@gmail.com>
2018-08-01 19:01:56 -0400

    quick update to discord.py

--- discord.py
+++ discord.py
@@ -72,6 +72,35 @@ async def on_ready():
 		breif='Verify yourself to allow access to this discord server',
 		aliases=['v','verifyme','verifyMe','VerifyMe','Verify'],
 		pass_context=True)
+async def verify(*args):
+	#await client.say(args[0].message.content) #debugging line
+	message = args[0].message.content
+
+	#!v, !v help
+	if ' ' not in message or 'help' in message:
+		await client.say('Welcome to the UMBC Overwatch Club Discord! To ensure the integrity of the club and access to this discord server we require all of our members to verify themselves with their emails. To verify your email use one of the following commands:'
+		+ '\n\t!verify <Your UMBC Campus ID>'
+		+ '\n\t!verify <Your UMBC UserID>' 
+		+ '\n\t!verify <Your @umbc.edu Email>'
+		+ '\n\nIf you are not a UMBC student or have any issues use the following command for an OWC admin to PM you with further instructions.')
+	#!v <umbc argument>
+	else:
+		await client.say('verifying ID... please hold') #debugging line
+		url = url_search + message.split()[1]
+		response = requests.get(url)
+
+		#valid ID if: has at least one digit, and only one result was found
+		if any(char.isdigit() for char in message.split()[1]) and '1 result found' in response.text:
+			#TODO remove the non-verified role
+			await client.say('Your UMBC status has been verified, welcome to the UMBC Overwatch Club')
+
+		elif '@' in message.split()[1]:
+			await client.say('Sorry, but we are unable to verify certain emails, you will be contacted by a server admin to complete your verification')
+			await client.send_message(client.get_channel(CHANNEL_VERIFICATION_ADMIN_ONLY_ID), 'Review required:\nUsername: ' + str(args[0].message.author) + '\nVerification tag: ' + args[0].message.content.split()[1])
+		else:
+			#TODO implement count system so @ 3 failures they are sent to the admins to be reviewed
+			await client.say('Sorry, either we could not verify your ID or something else unexpected went wrong. Please try again.\nverify_simple_search_check_error')
+	
 
 ''' Run '''
 if __name__ == '__main__':
