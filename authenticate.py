2a89c0330f9104a9efd5139f46a335e260018eec
JosephVolosin <joeyvolosin@gmail.com>
2018-08-03 16:56:46 -0400

    Updated checkUMBCStatus and added checkExistingAuth

--- authenticate.py
+++ authenticate.py
@@ -2,35 +2,63 @@
 #   directory
 # Using urllib bc i don't know how to use other things idk what would
 #   work best in this situation
-import urllib
-from urllib import request
+import requests
 
-# This is the base URL for searches, the URL changes to 
-#   DIRECTORY_URL + ?search='search here' when used, so just concat it and
-#   run another request
-#   ?search=josephv2%40umbc.edu is the URL when you search my e-mail, however
-#   it seems like feeding it '@' instead of '%40' gives the same results
+''' Constants '''
 DIRECTORY_URL = "https://www.umbc.edu/search/directory/?search="
-
-# checkUMBCStatus() sends a request to the website to show that it's available
-# Input:
-#   None
-# Output:
-#   0 = Fail
-#   1 = Success
-#   2 = Further contact needed
+'''
+ checkUMBCStatus() sends a request to the website to show that it's available
+ Input:
+   userMsg = string, user's username or ID
+ Output:
+   0 = Fail
+   1 = Success
+   2 = Further contact needed
+'''
 def checkUMBCStatus(userMsg):
+    
     url = DIRECTORY_URL + userMsg
+    resp = requests.get(url)
+    # Checks that search was valid
     try:
-        response = requests.get(url)
-    except:
-        print("Could not reach server.")
-        return 0 # This needs to have a error message that gets sent to the user
-    # Valid ID
-    if any(char.isdigit() for char in userMsg) and '1 result found' in response.text:
-        #TODO remove the non-verified role
-        await 1
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("Server could not be contacted, status code '" +
+                str(resp.status_code))
+    # Search results
+    if any(char.isdigit() for char in userMsg) and
+            '1 result found' in response.text:
+        return 1
     elif '@' in userMsg:
         return 2
     else:
         return 0
+
+'''
+ checkExistingAuth() checks the whitelist for the user's given username/ID
+ Input:
+    userName    = string, user's discord tag
+    userMsg     = string, user's username or ID
+ Output:
+    0 = Exists in whitelist
+    1 = Doesn't exist in whitelist
+    2 = Discord account is already authenticated
+'''
+def checkExistingAuth(userName, userMsg):
+    try:
+        whitelist = open('whitelist.txt')
+    except:
+        print("Whitelist could not be opened.")
+        return 0 # TODO - change this return to be it's own number
+    for l in whitelist.readlines():
+        if(userName in l):
+            print("Your discord account is already authenticated.")
+            return 2
+        elif(userMsg in l):
+            # TODO - Bot needs to message officers when it receives '2'
+            print("You have already used that e-mail/campus ID to authenticate \
+                    an account. A request will be sent to admins for furthur \
+                    review.")
+            return 2
+    return 1
+
