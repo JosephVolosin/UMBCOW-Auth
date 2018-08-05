e37e25226fe5b04d5663f29c1a6796b0839301f2
JosephVolosin <joeyvolosin@gmail.com>
2018-08-05 14:03:02 -0400

    Changed bot workflow to work around on_message. PMs now work.

--- umbc-bot.py
+++ umbc-bot.py
@@ -24,7 +24,6 @@ def fetchToken():
 		print("Error opening 'discord-keys.txt', FATAL.")
 		return 0
 
-# TODO - unneeded
 async def list_servers():
     await client.wait_until_ready()
     while not client.is_closed:
@@ -86,49 +85,80 @@ async def website(*args):
 		aliases=['v','verifyme','verifyMe','VerifyMe','Verify'],
 		pass_context=True)
 async def verify(*args):
-	#await client.say(args[0].message.content) #debugging line
+	author = args[0].message.author
 	message = args[0].message.content
-
-	#!v, !v help
-	if ' ' not in message or 'help' in message:
-		await client.say('Welcome to the UMBC Overwatch Club Discord! To ensure the integrity of the club and access to this discord server we require all of our members to verify themselves with their emails. To verify your email use one of the following commands:'
-		+ '\n\t!verify <Your UMBC Campus ID>'
-		+ '\n\t!verify <Your UMBC UserID>' 
-		+ '\n\t!verify <Your @umbc.edu Email>'
-		+ '\n\nIf you are not a UMBC student or have any issues use the following command for an OWC admin to PM you with further instructions.')
-	#!v <umbc argument>
-	else:
-		await client.say('verifying ID... please hold') #debugging line
-		# Check whitelist
-		res = authenticate.checkExistingAuth(str(args[0].message.author), message)
-		if(res == 0):
-			# Log to terminal TODO - create log file?
-			print("\t" + str(args[0].message.author) + ": sent an already used token.")
-			print("\t\t" + message)
-			await client.say("Sorry, your e-mail/ID has already been used to authenticate an account. Please contact an officer if this is wrong.")
-		elif(res == 2):
-			print("\t" + str(args[0].message.author) + ": attempted to authenticate an authenticated account.")
-			await client.say("It appears your current discord account is already authenticated.. If you are missing the role, please contact an officer.")
+	if(("!v" in message) or ("!verify" in message)):
+		#!v, !v help
+		if ' ' not in message or 'help' in message:
+			await client.say('Welcome to the UMBC Overwatch Club Discord! To ensure the integrity of the club and access to this discord server we require all of our members to verify themselves with their emails. To verify your email use one of the following commands:'
+			+ '\n\t!verify <Your UMBC Campus ID>'
+			+ '\n\t!verify <Your UMBC UserID>' 
+			+ '\n\t!verify <Your @umbc.edu Email>'
+			+ '\n\nIf you are not a UMBC student or have any issues use the following command for an OWC admin to PM you with further instructions.')
+		#!v <umbc argument>
 		else:
-			# Send request
-			res = authenticate.authenticateUser(message)
-			server = args[0].message.author.server
-			verified = discord.utils.get(server.roles, name='Verified')
-			unverified = discord.utils.get(server.roles, name='Unverified')
-			# Check result
+			await client.send_message(author, 'verifying ID... please hold') #debugging line
+			# Check whitelist
+			res = authenticate.checkExistingAuth(str(author), message)
 			if(res == 0):
-				print("Check failed.")
-				await client.say('Sorry, either we could not verify your ID or something else unexpected went wrong. Please try again.\nverify_simple_search_check_error')
-			elif(res == 1):
-				print("Check succeeded.")
-				await client.say("Your UMBC status has been verified, welcome to the UMBC Overwatch Club")
-				await client.add_roles(args[0].message.author, verified)
-				await client.remove_roles(args[0].message.author, unverified)
-				message = message[message.find(" ") + 1:]
-				whitelist.write(str(args[0].message.author) + "," + message)
+				# Log to terminal TODO - create log file?
+				print("\t" + str(author) + ": sent an already used token.")
+				print("\t\t" + message)
+				await client.send_message(author, "Sorry, your e-mail/ID has already been used to authenticate an account. Please contact an officer if this is wrong.")
 			elif(res == 2):
-				print("Further contact needed.")
-				await client.say("Sorry, but we are unable to verify certain emails, you will be contacted by a server admin to complete your verification")
+				print("\t" + str(author) + ": attempted to authenticate an authenticated account.")
+				await client.send_message(author, "It appears your current discord account is already authenticated.. If you are missing the role, please contact an officer.")
+			else:
+				# Send request
+				res = authenticate.authenticateUser(message)
+				server = client.get_server("455912302202322958")
+				verified = discord.utils.get(server.roles, name='Verified')
+				unverified = discord.utils.get(server.roles, name='Unverified')
+				# Check result
+				if(res == 0):
+					print("Check failed.")
+					await client.send_message(author, 'Sorry, either we could not verify your ID or something else unexpected went wrong. Please try again.\nverify_simple_search_check_error')
+				elif(res == 1):
+					print("Check succeeded.")
+					await client.send_message(author, "Your UMBC status has been verified, welcome to the UMBC Overwatch Club")
+					member = None
+					for mem in server.members:
+						if(mem == author):
+							member = mem
+					if(member == None):
+						print("The user is not a member of the server.")
+						return 0
+					await client.add_roles(member, verified)
+					await client.remove_roles(member, unverified)
+					message = message[message.find(" ") + 1:]
+					whitelist.write(str(author) + "," + message)
+				elif(res == 2):
+					print("Further contact needed.")
+					await client.send_message(author, "Sorry, but we are unable to verify certain emails, you will be contacted by a server admin to complete your verification")
+
+
+# on_member_join() messages a new user and tells them how to verify, all of 
+# verify will then be carried out in PM's
+@client.event
+async def on_member_join(*args):
+	
+	# Send user a PM greeting them and telling them to verify
+	print("Sending new user a message: " + str(args[0]))
+	print(type(args[0]))
+	client.send_message(args[0], "Welcome to the UMBC Overwatch server! We" +
+									"require users to authenticate their " +
+									"Discord accounts by using their campus ID " +
+									"or their username (ex: USERNAME@umbc.edu)" +
+									". Please send me a message in the format:" +
+									"\n`!v <CampusID>`\nor\n`!v <Username>`")
+	print("Message sent.")
+
+# on_message() will check the bot's PMs for command messages.
+@client.event
+async def on_message(*args):
+	message = args[0].content
+	await client.process_commands(args[0])
+
 
 # TODO - Remove on deploy build, this is for debugging only
 @client.command(name="stop",
@@ -137,6 +167,7 @@ async def verify(*args):
 async def stop(*args):
 	await client.logout()
 	print("Ending client..")
+
 
 ''' Run '''
 if __name__ == '__main__':
