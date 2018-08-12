ec8d42fe53f9b124baab5fe7df13ee44027b4398
JosephVolosin <joeyvolosin@gmail.com>
2018-08-11 22:15:18 -0400

    Fixing up format in the visitor file

--- visitor.py
+++ visitor.py
@@ -11,7 +11,7 @@ TIME_PATTERN = "%Y%m%d %H:%M:%S"
 def removeOldVisitors():
 
     now_time_stamp = datetime.datetime.now()
-    now_time_stamp = datetime.datetime.strftime(now_time_stamp, TIME_PATTERN)
+    now_time_stamp = datetime.datetime.strftime(now_time_stamp.rstrip(), TIME_PATTERN)
     f = open(FN, 'r')
     for l in f:
         l_temp = l.split(",")[2]
