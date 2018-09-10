8a13cc7e7db7c223c2aaa0266f105e4889390246
Joseph Volosin <joeyvolosin@gmail.com>
2018-09-09 23:46:09 -0400

    No idea what I was doing in whitelist.py but it's fixed so that the whitelist doesn't grow exponentially

--- whitelist.py
+++ whitelist.py
@@ -6,21 +6,13 @@ FN = "whitelist.txt"
 
 # write(newAddition) adds newAddition to the whitelist
 def write(newAddition):
-    
-    # Capture all old lines to be re-written
-    old_lines = ''
-    with open(FN, 'r') as f:
-        for l in f:
-            old_lines += l + "\n"
-        f.close()
-
-    # Write all lines
-    with open(FN, 'w') as f:
-        f.write(old_lines)
+    print("Writing " + newAddition + " to the whitelist..")
+    with open(FN, 'a') as f:
         f.write(newAddition + "\n")
         f.close()
     
 # clear() removes all lines from the file
 def clear():
+    print("Clearing whitelist..")
     f = open(FN, 'w')
     f.close()
